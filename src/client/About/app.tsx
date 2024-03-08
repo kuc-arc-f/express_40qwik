0946adab1e9c1eec2ce86afe3cc5a1b15d47733f
_ñ <knaka0209@gmail.com>
2024-03-08 20:16:22 +0900

    up, 0308a15

--- src/client/About/app.tsx
+++ src/client/About/app.tsx
@@ -8,9 +8,9 @@ export const App = component$(() => {
   return (
   <>
     <Head />
-    <div class="container mx-auto my-2 px-8 bg-white">
-      
-      <h1>About!!</h1>
+    <div class="container mx-auto my-2 px-8 bg-white">  
+      <h1 class="text-4xl font-bold">About!!!</h1>
+      <hr />
       <div class="card">
         <button onClick$={() => count.value++}
         >[ count ] is {count.value}</button>
